0ed328fa41c273b3b0a1549aa456a6d40e952893
daprionil <davidarenaoflol23@gmail.com>
2023-11-02 07:33:13 -0500

    Set dependencies

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        '*.{html,js,jsx}',
+        '**/*.{html,js,jsx}',
+        '**/**/*.{html,js,jsx}',
+        '../index.html'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
