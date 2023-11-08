59fa8234a8c29808145720a246b5d3e77b963cee
daprionil <davidarenaoflol23@gmail.com>
2023-11-07 21:18:15 -0500

    Create smtp transporter

---
+++ server/src/config/smtpTransport.js
@@ -0,0 +1,19 @@
+const nodemailer = require('nodemailer');
+
+const {
+    SMTP_HOST,
+    SMTP_PORT,
+    SMTP_USER,
+    SMTP_PASS
+} = process.env;
+
+const transporter = nodemailer.createTransport({
+    host: SMTP_HOST,
+    port: SMTP_PORT,
+    auth:{
+        user: SMTP_USER,
+        pass: SMTP_PASS
+    }
+});
+
+module.exports = transporter;
