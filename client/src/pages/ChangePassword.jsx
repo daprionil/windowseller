2f782303e4cd7cf36c714b593a3273424fe02f3a
daprionil <davidarenaoflol23@gmail.com>
2023-11-16 10:11:29 -0500

    Create form styles to changePassword and the structure

--- client/src/pages/ChangePassword.jsx
+++ client/src/pages/ChangePassword.jsx
@@ -1,6 +1,69 @@
+import { useEffect, useState } from "react"
+import HeaderLanding from "../components/LandingPage/HeaderLanding"
+import { Formik } from "formik";
+
 const ChangePassword = () => {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        console.log(loading);
+    }, [])
+
     return (
-        <div>ChangePassword</div>
+        <>
+            <HeaderLanding />
+            <div className="grid mt-10 py-10 px-5 grid-cols-1 items-start lg:grid-cols-2">
+                <div className="py-8 px-6 max-w-lg lg:max-w-none text-center mx-auto">
+                    <p className="font-black text-4xl lg:text-6xl">
+                        ¡Es tu momento! Usa una que puedas <span className="text-yellow-500">Recordar!</span>
+                    </p>
+                </div>
+                <div className="shadow-xl  rounded-md max-w-md lg:max-w-none p-2 lg:mx-0 mx-auto">
+                    <Formik
+                        initialValues={{
+                            password: '',
+                            repeatpassword: ''
+                        }}
+                        
+                    >
+                        {({ values: { password, repeatpassword }, handleSubmit, isSubmitting, handleChange }) => (
+                            <form
+                                onSubmit={handleSubmit}
+                                className="px-6 py-8 space-y-5"
+                            >
+                                <h1 className="text-center font-black">Cambia tu Contraseña</h1>
+                                <label className="block">
+                                    <p className="font-bold">Contraseña</p>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        value={password}
+                                        onChange={handleChange}
+                                        placeholder="Nueva Contraseña"
+                                    />
+                                </label>
+                                <label className="block">
+                                    <p className="font-bold">Repite tu Contraseña</p>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        value={repeatpassword}
+                                        onChange={handleChange}
+                                        placeholder="Confirma tu nueva contraseña"
+                                    />
+                                </label>
+                                <button
+                                    type="submit"
+                                    disabled={isSubmitting}
+                                    className="btn_base text-white font-bold"
+                                    style={{ background: "rgb(77, 69, 61)" }}
+                                >Reestablecer</button>
+                            </form>
+                        )}
+                    </Formik>
+                </div>
+            </div>
+        </>
     )
 }
 
--- client/src/pages/LandingPage/LogInPage.jsx
+++ client/src/pages/LandingPage/LogInPage.jsx
@@ -3,10 +3,10 @@ import LogInForm from "../../components/LandingPage/LogInForm"
 const LogInPage = () => {
     return (
         <div className='block mt-10 mx-auto space-y-5 lg:grid lg:grid-cols-2 lg:max-w-5xl lg:items-center lg:space-x-6'>
-            <div className=''>
-                <h1 className=' text-4xl lg:text-6xl drop-shadow-xl font-black text-center'>
+            <div>
+                <p className=' text-4xl lg:text-6xl drop-shadow-xl font-black text-center'>
                     Inicia sesión y <span className='text-yellow-500'>Administra</span> tus Catálogos
-                </h1>
+                </p>
             </div>
             <div className='shadow-xl rounded overflow-hidden p-2 w-full max-w-md lg:max-w-none mx-auto'>
                 <LogInForm />
