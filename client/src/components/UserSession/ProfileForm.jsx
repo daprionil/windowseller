69d7e45cbde6429196762889f610bab93d31baf8
daprionil <davidarenaoflol23@gmail.com>
2023-11-24 22:32:47 -0500

    Fixed the profile view

--- client/src/components/UserSession/ProfileForm.jsx
+++ client/src/components/UserSession/ProfileForm.jsx
@@ -3,125 +3,134 @@ import { Formik } from "formik"
 import Message, { typeMessages } from "../Message.jsx";
 import { errorsFieldValidations, fieldValidations, nameFields } from "../../utils/formValidations";
 import Loader from "../Loader";
+import useGetUserData from "../../hooks/useGetUserData.jsx";
 
-const ProfileForm = ({namecompany,eslogan,description,email,phone}) => {
+const ProfileForm = () => {
+    const [loading, user] = useGetUserData();
+
     return (
-        <Formik
-            initialValues={{
-                namecompany,
-                eslogan,
-                description,
-                phone,
-            }}
-            validate={(values) => {
-                let errors = [];
-                const listValues = Object.entries(values);
-                for(let [key,val] of listValues){
-                    const currentValue = (val + '').trim();
-                    if(!currentValue){
-                        errors.push([key,`${nameFields[key]} no puede estar vacío`]);
-                        continue;
-                    }
-                    if(currentValue.length < 8){
-                        errors.push([key,`${nameFields[key]} debe contener más de 8 carácteres`]);
-                        continue;
-                    }
-                    const validation = fieldValidations[key];
-                    if(validation){
-                        const resultValidation = validation(val);
-                        if(!resultValidation){
-                            errors.push([key, errorsFieldValidations[key]]);
+        loading ?
+            <div className="py-5 text-center">
+                <Loader />
+            </div>
+            : user === null || user?.error ?
+                <Message msg="Ha habido un problema al cargar el formulario" type={typeMessages.ERROR} />
+                : <Formik
+                    initialValues={{
+                        namecompany: user.namecompany,
+                        eslogan: user.eslogan,
+                        description: user.description,
+                        phone: user.phone,
+                    }}
+                    validate={(values) => {
+                        let errors = [];
+                        const listValues = Object.entries(values);
+                        for (let [key, val] of listValues) {
+                            const currentValue = (val + '').trim();
+                            if (!currentValue) {
+                                errors.push([key, `${nameFields[key]} no puede estar vacío`]);
+                                continue;
+                            }
+                            if (currentValue.length < 8) {
+                                errors.push([key, `${nameFields[key]} debe contener más de 8 carácteres`]);
+                                continue;
+                            }
+                            const validation = fieldValidations[key];
+                            if (validation) {
+                                const resultValidation = validation(val);
+                                if (!resultValidation) {
+                                    errors.push([key, errorsFieldValidations[key]]);
+                                }
+                                continue;
+                            }
                         }
-                        continue;
-                    }
-                }
 
-                return Object.fromEntries(errors);
-            }}
-            onSubmit={(data) => {
-                console.log(data);
-            }}
-        >
-            {({handleSubmit, handleChange, isSubmitting, values, errors}) => (
-                <form
-                    className="space-y-2 [&>label>input]:bg-stone-200"
-                    onSubmit={handleSubmit}
+                        return Object.fromEntries(errors);
+                    }}
+                    onSubmit={(data) => {
+                        console.log(data);
+                    }}
                 >
-                    <label className="block w-full">
-                        <p className="pb-2 pl-1 font-bold">Compañía</p>
-                        <input
-                            type="text"
-                            name="namecompany"
-                            onChange={handleChange}
-                            value={values.namecompany}
-                            placeholder="Nombre de compañía"
-                        />
-                        <div className="text-sm">
+                    {({ handleSubmit, handleChange, isSubmitting, values, errors }) => (
+                        <form
+                            className="space-y-2 [&>label>input]:bg-stone-200"
+                            onSubmit={handleSubmit}
+                        >
+                            <label className="block w-full">
+                                <p className="pb-2 pl-1 font-bold">Compañía</p>
+                                <input
+                                    type="text"
+                                    name="namecompany"
+                                    onChange={handleChange}
+                                    value={values.namecompany}
+                                    placeholder="Nombre de compañía"
+                                />
+                                <div className="text-sm">
+                                    {
+                                        errors.namecompany && <Message msg={errors.namecompany} type={typeMessages.ERROR} />
+                                    }
+                                </div>
+                            </label>
+                            <label className="block w-full">
+                                <p className="pb-2 pl-1 font-bold">Lema empresarial</p>
+                                <input
+                                    type="text"
+                                    name="eslogan"
+                                    onChange={handleChange}
+                                    value={values.eslogan}
+                                    placeholder="Impacta con tu eslogan!"
+                                />
+                                <div className="text-sm">
+                                    {
+                                        errors.eslogan && <Message msg={errors.eslogan} type={typeMessages.ERROR} />
+                                    }
+                                </div>
+                            </label>
+                            <label className="block w-full">
+                                <p className="pb-2 pl-1 font-bold">Descripción</p>
+                                <textarea
+                                    name="description"
+                                    onChange={handleChange}
+                                    value={values.description}
+                                    placeholder="Describe la Esencia de tu compañía..."
+                                ></textarea>
+                                <div className="text-sm">
+                                    {
+                                        errors.description && <Message msg={errors.description} type={typeMessages.ERROR} />
+                                    }
+                                </div>
+                            </label>
+                            <label className="block w-full">
+                                <p className="pb-2 pl-1 font-bold">Contacto</p>
+                                <input
+                                    type="number"
+                                    name="phone"
+                                    onChange={handleChange}
+                                    value={values.phone}
+                                    className="rounded-md"
+                                    placeholder='Ej: (+57)3202999999'
+                                />
+                                <div className="text-sm">
+                                    {
+                                        errors.phone && <Message msg={errors.phone} type={typeMessages.ERROR} />
+                                    }
+                                </div>
+                            </label>
                             {
-                                errors.namecompany && <Message msg={errors.namecompany} type={typeMessages.ERROR} />
+                                isSubmitting ?
+                                    <div className="text-right pr-14 pt-6 pb-2">
+                                        <Loader />
+                                    </div>
+                                    : <div className="w-full text-right [&>button]:bg-blue-500 [&>button]:text-white [&>button]:font-black">
+                                        <button
+                                            type="submit"
+                                            className="btn_base btn_base_hover before:bg-transparent"
+                                        >Guardar Cambios</button>
+                                    </div>
                             }
-                        </div>
-                    </label>
-                    <label className="block w-full">
-                        <p className="pb-2 pl-1 font-bold">Lema empresarial</p>
-                        <input
-                            type="text"
-                            name="eslogan"
-                            onChange={handleChange}
-                            value={values.eslogan}
-                            placeholder="Impacta con tu eslogan!"
-                        />
-                        <div className="text-sm">
-                            {
-                                errors.eslogan && <Message msg={errors.eslogan} type={typeMessages.ERROR} />
-                            }
-                        </div>
-                    </label>
-                    <label className="block w-full">
-                        <p className="pb-2 pl-1 font-bold">Descripción</p>
-                        <textarea
-                            name="description"
-                            onChange={handleChange}
-                            value={values.description}
-                            placeholder="Describe la Esencia de tu compañía..."
-                        ></textarea>
-                        <div className="text-sm">
-                            {
-                                errors.description && <Message msg={errors.description} type={typeMessages.ERROR} />
-                            }
-                        </div>
-                    </label>
-                    <label className="block w-full">
-                        <p className="pb-2 pl-1 font-bold">Contacto</p>
-                        <input
-                            type="number"
-                            name="phone"
-                            onChange={handleChange}
-                            value={values.phone}
-                            className="rounded-md"
-                            placeholder='Ej: (+57)3202999999'
-                        />
-                        <div className="text-sm">
-                            {
-                                errors.phone && <Message msg={errors.phone} type={typeMessages.ERROR} />
-                            }
-                        </div>
-                    </label>
-                    {
-                        isSubmitting ?
-                            <div className="text-right pr-14 pt-6 pb-2">
-                                <Loader />
-                            </div>
-                        :   <div className="w-full text-right [&>button]:bg-blue-500 [&>button]:text-white [&>button]:font-black">
-                                <button
-                                    type="submit"
-                                    className="btn_base btn_base_hover before:bg-transparent"
-                                >Guardar Cambios</button>
-                            </div>
-                    }
-                </form>
-            )}
-        </Formik>
+                        </form>
+                    )}
+                </Formik>
     )
 }
 
--- client/src/pages/UserSession/ProfileUser.jsx
+++ client/src/pages/UserSession/ProfileUser.jsx
@@ -2,13 +2,10 @@ import { useState } from "react";
 import { IoBusiness } from 'react-icons/io5';
 import { RiListSettingsLine } from 'react-icons/ri';
 
-import Loader from "../../components/Loader";
-import useGetUserData from "../../hooks/useGetUserData";
-import formatDate from "../../utils/formatDate";
 import ProfileForm from "../../components/UserSession/ProfileForm";
+import ProfileViewDataUser from "../../components/UserSession/ProfileViewDataUser";
 
 const ProfileUser = () => {
-    const [loading, user] = useGetUserData();
     const [profileMode, setProfileMode] = useState(false);
 
     return (
@@ -17,83 +14,36 @@ const ProfileUser = () => {
                 <h1 className="font-black text-lg">Mi Perfil</h1>
             </div>
             <div className="mt-6 bg-white shadow-xl rounded-md overflow-hidden h-full">
-                {
-                    loading ?
-                        <div className="text-center py-4 px-4">
-                            <Loader />
-                        </div>
-                        : user === null || user?.error ?
-                            <p>{user?.error ?? 'Ha habido un error al cargar tu usuario'}</p>
-                            : <div className="">
-                                <div className="grid grid-cols-2">
-                                    <div
-                                        className={`p-4 flex items-center cursor-pointer justify-center text-slate-100 text-3xl [&>svg]:drop-shadow-md ${!profileMode ? "bg-slate-500" : 'bg-slate-300'}`}
-                                        onClick={() => setProfileMode(false)}
-                                    >
-                                        <IoBusiness />
-                                    </div>
-                                    <div
-                                        className={`p-4 flex items-center cursor-pointer justify-center text-slate-100 text-3xl [&>svg]:drop-shadow-md ${profileMode ? "bg-slate-500" : 'bg-slate-300'}`}
-                                        onClick={() => setProfileMode(true)}
-                                    >
-                                        <RiListSettingsLine />
-                                    </div>
-                                </div>
-                                <div className="p-8">
-                                {
-                                    profileMode ?
-                                        <div className="relative">
-                                            <ProfileForm {...user} />
-                                            <button
-                                                className="bottom-0 left-0 absolute btn_base bg-red-500 text-white font-black"
-                                                onClick={() => setProfileMode(false)}
-                                            >
-                                                Cancelar
-                                            </button>
-                                        </div>
-                                    :
-                                        <>
-                                            <div className="py-2 space-y-2">
-                                                <p className="font-bold">Nombre de compañía</p>
-                                                <div className="px-2 py-1 rounded-md bg-stone-100 shadow font-thin">
-                                                    {user.namecompany}
-                                                </div>
-                                            </div>
-                                            <div className="py-2 space-y-2">
-                                                <p className="font-bold">Eslogan</p>
-                                                <div className="px-2 py-1 rounded-md bg-stone-100 shadow font-thin">
-                                                    {user.eslogan}
-                                                </div>
-                                            </div>
-                                            <div className="py-2 space-y-2">
-                                                <p className="font-bold">Descripción</p>
-                                                <div className="px-2 py-1 rounded-md bg-stone-100 shadow font-thin">
-                                                    {user.description}
-                                                </div>
-                                            </div>
-                                            <div className="py-2 space-y-2">
-                                                <p className="font-bold">Correo Electrónico</p>
-                                                <div className="px-2 py-1 rounded-md bg-stone-100 shadow font-thin">
-                                                    {user.email}
-                                                </div>
-                                            </div>
-                                            <div className="py-2 space-y-2">
-                                                <p className="font-bold">Número telefónico</p>
-                                                <div className="px-2 py-1 rounded-md bg-stone-100 shadow font-thin">
-                                                    {user.phone}
-                                                </div>
-                                            </div>
-                                            <div className="py-2 space-y-2">
-                                                <p className="font-bold">Última actualización de datos</p>
-                                                <div className="px-2 py-1 rounded-md bg-stone-100 shadow font-thin">
-                                                    {formatDate(user.updatedAt)}
-                                                </div>
-                                            </div>
-                                        </>
-                                }
-                                </div>
+                <div className="grid grid-cols-2">
+                    <div
+                        className={`p-4 flex items-center cursor-pointer justify-center text-slate-100 text-3xl [&>svg]:drop-shadow-md ${!profileMode ? "bg-slate-500" : 'bg-slate-300'}`}
+                        onClick={() => setProfileMode(false)}
+                    >
+                        <IoBusiness />
+                    </div>
+                    <div
+                        className={`p-4 flex items-center cursor-pointer justify-center text-slate-100 text-3xl [&>svg]:drop-shadow-md ${profileMode ? "bg-slate-500" : 'bg-slate-300'}`}
+                        onClick={() => setProfileMode(true)}
+                    >
+                        <RiListSettingsLine />
+                    </div>
+                </div>
+                <div className="p-8">
+                    {
+                        profileMode ?
+                            <div className="relative">
+                                <ProfileForm />
+                                <button
+                                    className="bottom-0 left-0 absolute btn_base bg-red-500 text-white font-black"
+                                    onClick={() => setProfileMode(false)}
+                                >
+                                    Cancelar
+                                </button>
                             </div>
-                }
+                            :
+                            <ProfileViewDataUser />
+                    }
+                </div>
             </div>
         </div>
     )
