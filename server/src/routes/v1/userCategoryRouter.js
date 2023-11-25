2aa816fe25b5d2131b121cdf50d340dece52e7c8
daprionil <davidarenaoflol23@gmail.com>
2023-11-25 15:05:37 -0500

    Create route to update a Category by object user PUT/v1/users/category/:categoryId

--- server/src/routes/v1/userCategoryRouter.js
+++ server/src/routes/v1/userCategoryRouter.js
@@ -2,13 +2,15 @@ const { Router } = require('express');
 const getCategoryHandler = require('./handlers/userCategoryHandlers/getCategoryHandler');
 const createCategoryHandler = require('./handlers/userCategoryHandlers/createCategoryHandler');
 const getAllCategoriesHandler = require('./handlers/userCategoryHandlers/getAllCategoriesHandler');
+const updateACategoryHandler = require('./handlers/userCategoryHandlers/updateACategoryHandler');
 
 const userCategoryRouter = Router();
 
 //* Generate routes
 userCategoryRouter.get('/:categoryId', getCategoryHandler);
-userCategoryRouter.post('/', createCategoryHandler);
 userCategoryRouter.get('/', getAllCategoriesHandler);
+userCategoryRouter.post('/', createCategoryHandler);
+userCategoryRouter.put('/:categoryId', updateACategoryHandler);
 
 //? Export the router
 module.exports = userCategoryRouter;
