392675513ed5c4c373366184b078b4e3af0be3f2
daprionil <davidarenaoflol23@gmail.com>
2023-12-04 05:36:41 -0500

    Create route to get a Product GET/v1/users/products/:productId

--- server/src/routes/v1/handlers/userProductsHandlers/createProductHandler.js
+++ server/src/routes/v1/handlers/userProductsHandlers/createProductHandler.js
@@ -1,6 +1,12 @@
 module.exports = async function(req, res){
     try {
-        res.send('Done')
+        const { name, description, price, image } = req.body;
+        const objectUser = res.locals.userAuthorizate;
+
+        //* Validate necesary values to create a product
+        console.log({ name, description, price, image });
+        
+        res.send('Done');
     } catch ({status, message}) {
         res.status(status ?? 500).json({
             error: message
--- server/src/routes/v1/handlers/userProductsHandlers/getAProductHandler.js
+++ server/src/routes/v1/handlers/userProductsHandlers/getAProductHandler.js
@@ -1,6 +1,21 @@
+const getAProductById = require("../../../../controllers/products/getAProductById");
+const CustomErrors = require("../../../../utils/errors/CustomErrors");
+
 module.exports = async function(req, res){
     try {
-        res.send('Done')
+        const { productId } = req.params;
+        const objectUser = res.locals.userAuthorizate;
+
+        //* Validate the value from productId
+        if(isNaN(productId)){
+            throw CustomErrors.SintaxError('El idenficador no tiene un formato válido');
+        }
+
+        //* get a product
+        const productFound = await getAProductById( productId, objectUser );
+        console.log(productFound);
+
+        res.send('Done');
     } catch ({status, message}) {
         res.status(status ?? 500).json({
             error: message
