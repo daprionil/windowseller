2135a7ed2be7cc40624978d13fa52ca0e179b233
daprionil <davidarenaoflol23@gmail.com>
2023-12-04 23:09:43 -0500

    Create route to create Products POST/v1/users/products/

--- server/src/controllers/products/createProduct.js
+++ server/src/controllers/products/createProduct.js
@@ -1,3 +1,7 @@
-module.exports = async function(){
+module.exports = async function({ name, description, price, image },objectUser){
+    const dataToCreate = { name, description, price, image };
+    const createdProduct = await objectUser.createProduct(dataToCreate);
     
-}
+    const { UserId, ...rest } = createdProduct.dataValues;
+    return rest;
+};
--- server/src/routes/v1/handlers/userProductsHandlers/createProductHandler.js
+++ server/src/routes/v1/handlers/userProductsHandlers/createProductHandler.js
@@ -1,12 +1,36 @@
+const createProduct = require("../../../../controllers/products/createProduct");
+const CustomErrors = require("../../../../utils/errors/CustomErrors");
+const { validations } = require("../../../../utils/validationFormFields");
+
 module.exports = async function(req, res){
     try {
         const { name, description, price, image } = req.body;
         const objectUser = res.locals.userAuthorizate;
 
-        //* Validate necesary values to create a product
-        console.log({ name, description, price, image });
-        
-        res.send('Done');
+        //* Validate if exist the necesary values to create a product
+        const emptyValues = Object.entries(
+            {name, description, price, image}
+        ).some(([,v]) => !(v+"").trim());
+        if(emptyValues){
+            throw CustomErrors.SintaxError('Faltan campos por recibir para la creación de producto');
+        };
+
+        //* Validate image URL format
+        const validationImageUrl = validations.url(image);
+        if(!validationImageUrl){
+            throw CustomErrors.SintaxError('La imágen no tiene un formato válido');
+        };
+
+        //* Validate price value
+        const validationPrice = /^([0-9]\d+){1,10}$/g.test(price);
+        if(!validationPrice){
+            throw CustomErrors.SintaxError('El precio no contiene un formato válido');
+        }
+
+        //? Create the product
+        const createdProduct = await createProduct({ name, description, price, image }, objectUser);
+
+        res.json({ createdProduct });
     } catch ({status, message}) {
         res.status(status ?? 500).json({
             error: message
