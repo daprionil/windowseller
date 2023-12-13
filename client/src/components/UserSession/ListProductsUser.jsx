93b5302cc3147d3dc24cffb35f620c54963b169d
daprionil <davidarenaoflol23@gmail.com>
2023-12-12 20:39:17 -0500

    Fixed List Products component - [If the list List Products is empty]

--- client/src/components/UserSession/ListProductsUser.jsx
+++ client/src/components/UserSession/ListProductsUser.jsx
@@ -1,13 +1,24 @@
+import useProductUserStore from '../../stores/useProductUserStore';
 import ProductCard from './ProductCard';
 
-const ListProductsUser = ({userProducts}) => {
+const ListProductsUser = ({handleCreateProduct}) => {
+    const userProducts = useProductUserStore(({ userProducts }) => userProducts);
     return (
         <div className='space-y-2'>
             {
                 userProducts.length ? userProducts.map((product, idx) => (
                     <ProductCard {...product} key={idx}/>
                 ))
-                    : <p>No hay productos</p>
+                    :   <div className='bg-white shadow-md rounded-md text-center px-10 pt-10 pb-8 space-y-2'>
+                            <p className='font-black text-2xl'>¡ No hay <span className='text-slate-400'>Productos !</span></p>
+                            <p className='underline'>Crea productos y compartelos en tus catálogos</p>
+                            <button
+                                className='btn_base shadow bg-green-500 text-white font-bold btn_base_hover'
+                                onClick={handleCreateProduct}
+                            >
+                                Crear Producto
+                            </button>
+                        </div>
             }
         </div>
     )
